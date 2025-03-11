0947e4daf1c20f4cb25a43162fa8d48e32f8c3bf
PrithviChauhan06 <162803707+PrithviChauhan06@users.noreply.github.com>
2025-03-11 21:50:30 +0530

    updated

---
+++ FactorialRec.java
@@ -0,0 +1,11 @@
+public class FactorialRec {
+    public static int Factorial(int n){
+        if(n==1){
+            return 1;
+        }
+        return n*Factorial(n-1);
+    }
+    public static void main(String[] args) {
+        System.out.println(Factorial(5));
+    }
+}
